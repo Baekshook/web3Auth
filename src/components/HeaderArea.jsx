22fa915b565b9f6e3b6330baf9a2f709de0578ea
Baekshook <ecgo0106@naver.com>
2024-04-03 14:24:14 +0900

    css design fix 0403

--- src/components/HeaderArea.jsx
+++ src/components/HeaderArea.jsx
@@ -1,31 +1,57 @@
-import PropTypes from "prop-types";
 import React from "react";
-import "./Header.css";
+import styled from "styled-components";
 
-export const HeaderArea = ({
-  className,
-  divClassName,
-}) => {
+const HeaderWrapper = styled.div`
+  background-image: url(../image/headerArea.png);
+  background-size: 100% 100%;
+  height: 50px;
+  position: relative;
+  width: 318px;
+`;
+
+const TextWrapper = styled.div`
+  color: #b9f8fb;
+  font-family: "Nico Moji", Helvetica;
+  font-size: 14px;
+  font-weight: 400;
+  height: 14px;
+  left: 18px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  top: 17px;
+  white-space: nowrap;
+`;
+
+const Icon = styled.img`
+  margin-top: 5px;
+  height: 17px;
+  left: ${({ left }) => left || "0"};
+  position: absolute;
+  top: ${({ top }) => top || "0"};
+  width: 17px;
+`;
+
+export const HeaderArea = ({ className, divClassName }) => {
   return (
-    <div className={`header-area ${className}`}>
-      <div className={`text-wrapper ${divClassName}`}>
+    <HeaderWrapper className={`header-area ${className}`}>
+      <TextWrapper className={`text-wrapper ${divClassName}`}>
         @WE-ING with_ Wepublic
-      </div>
-      <img
+      </TextWrapper>
+      <Icon
         className="iconly-broken-chat"
         alt="Iconly broken chat"
-        src="/image/vector.png"
+        src="/image/Chat.png"
+        left="249px"
+        top="13px"
       />
-      <img
-        className="iconly-broken-chat"
+      <Icon
+        className="iconly-broken-chat additional-icons"
         alt="Iconly broken chat"
         src="/image/vector.png"
+        left="276px"
+        top="13px"
       />
-    </div>
+    </HeaderWrapper>
   );
-};
-
-HeaderArea.propTypes = {
-  iconlyBrokenChat: PropTypes.string,
-  iconShareAltComponentAdditionalIconsColor: PropTypes.string,
 };
